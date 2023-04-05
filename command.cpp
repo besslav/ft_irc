aa4d27e3f26330a97aca2458f63a42798be8b57e
besslav <duma.v2703rus@yandex.ru>
2023-04-05 19:41:48 +0300

    autorise_base

---
+++ command.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Price Skip on 4/5/23.
+//
+
+#include "command.hpp"
---
+++ command.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Price Skip on 4/5/23.
+//
+
+#ifndef FT_IRC_COMMAND_HPP
+#define FT_IRC_COMMAND_HPP
+
+
+
+class command {
+
+};
+
+
+
+#endif //FT_IRC_COMMAND_HPP
